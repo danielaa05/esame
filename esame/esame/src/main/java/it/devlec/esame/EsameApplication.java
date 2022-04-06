2f9ce026d31db587a5d2a11b307dbbedd8fe2a08
danielaa05 <daniela2madaro@gmail.com>
2022-04-06 11:45:37 +0200

    primo inserimento

---
+++ esame/esame/src/main/java/it/devlec/esame/EsameApplication.java
@@ -0,0 +1,13 @@
+package it.devlec.esame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameApplication.class, args);
+	}
+
+}
